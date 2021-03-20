470373f392c57cda29ed58f3c9e090f2ee5d8be9
yiranzai <wuqingdzx@gmail.com>
2021-03-20 22:37:19 +0800

    每日一练2021-3-20

--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -203,3 +203,47 @@ func deepWordBreak(s string, last int) bool {
 	dp[start+length] = 2
 	return false
 }
+
+func wordBreak2(s string, wordDict []string) bool {
+	// f[i] 表示前i个字符是否可以被切分
+	// f[i] = f[j] && s[j+1~i] in wordDict
+	// f[0] = true
+	// return f[len]
+
+	if len(s) == 0 {
+		return true
+	}
+	f := make([]bool, len(s)+1)
+	f[0] = true
+	max, dict := maxLen(wordDict)
+	for i := 1; i <= len(s); i++ {
+		l := 0
+		if i-max > 0 {
+			l = i - max
+		}
+		for j := l; j < i; j++ {
+			if f[j] && inDict(s[j:i], dict) {
+				f[i] = true
+				break
+			}
+		}
+	}
+	return f[len(s)]
+}
+
+func maxLen(wordDict []string) (int, map[string]bool) {
+	dict := make(map[string]bool)
+	max := 0
+	for _, v := range wordDict {
+		dict[v] = true
+		if len(v) > max {
+			max = len(v)
+		}
+	}
+	return max, dict
+}
+
+func inDict(s string, dict map[string]bool) bool {
+	_, ok := dict[s]
+	return ok
+}
